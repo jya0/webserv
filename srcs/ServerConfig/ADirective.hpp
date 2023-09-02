7bd8e62c05d7905c47056249ad2a346074c30b3c
Jinxiu Yao <jyao@student.42abudhabi.ae>
2023-09-02 13:15:00 +0400

    feat: finish implementation of ADirective abstract class

--- srcs/ServerConfig/ADirective.hpp
+++ srcs/ServerConfig/ADirective.hpp
@@ -6,7 +6,7 @@
 /*   By: jyao <jyao@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 09:50:31 by jyao              #+#    #+#             */
-/*   Updated: 2023/09/02 12:27:38 by jyao             ###   ########.fr       */
+/*   Updated: 2023/09/02 13:13:49 by jyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include	<string>
 # include	<vector>
+# include	"ServerConfig_namespace.hpp"
 
 /**
  * @brief The abstract class inherited by DirectiveBlock and DirectiveSimple
@@ -22,17 +23,19 @@
 class	ADirective {
 	protected:
 		std::string	_dveName;	///used as the key for the map of Directive
-		std::vector< std::string >	_dveValues;	///values for 
-		std::string	_dveType;
+		std::vector< std::string >	_dveValues;	///used as the key for the map of Directive
+		serverConfig::DirectiveType	_dveType;	///enum DirectiveType is declared in the serverConfig namespace
 	public:
+		/*Canonical Class Form*/
 		ADirective(void);
 		ADirective(ADirective const	&dveREF);
 		virtual	~ADirective(void);
 		ADirective	&operator=(ADirective const	&dveREF);
 
+		/*Accessors*/
 		std::string	getName(void) const;
 		std::vector< std::string >	getValues(void) const;
-		std::string	getType(void) const;
+		serverConfig::DirectiveType	getType(void) const;
 		void	setName(std::string const	&strREF);
 		void	setValues(std::vector< std::string > const	&strsREF);
 };
--- srcs/ServerConfig/ServerConfig_namespace.hpp
+++ srcs/ServerConfig/ServerConfig_namespace.hpp
@@ -1,13 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server_config_namespace.hpp                        :+:      :+:    :+:   */
+/*   ServerConfig_namespace.hpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jyao <jyao@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 09:48:06 by jyao              #+#    #+#             */
-/*   Updated: 2023/09/02 09:49:04 by jyao             ###   ########.fr       */
+/*   Updated: 2023/09/02 13:03:21 by jyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-namespace 
+#ifndef SERVER_CONFIG_NAMESPACE_HPP
+# define SERVER_CONFIG_NAMESPACE_HPP
+
+# include	"ADirective.hpp"
+# include	"DirectiveSimple.hpp"
+# include	"DirectiveBlock.hpp"
+
+namespace	serverConfig {
+	enum class	DirectiveType {
+		INIT = -1,
+		AUTOINDEX,
+		CLIENT_MAX_BODY_SIZE,
+		ERROR_PAGE,
+		INDEX,
+		LIMIT_EXCEPT,
+		LISTEN,
+		LOCATION,
+		RETURN,
+		ROOT,
+		SERVER_NAME
+	};
+}
+
+#endif
