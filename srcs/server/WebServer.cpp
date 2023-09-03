12d1b48323c35c376a43e892bcd6485a19e384c3
kalmheir <zunon@zunon.io>
2023-09-03 10:10:47 +0400

    fix(WebServer): bring back my deleted code

--- srcs/server/WebServer.cpp
+++ srcs/server/WebServer.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   WebServer.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rriyas <rriyas@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kalmheir <kalmheir@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 17:55:39 by rriyas            #+#    #+#             */
-/*   Updated: 2023/09/02 22:03:55 by rriyas           ###   ########.fr       */
+/*   Updated: 2023/09/03 10:10:19 by kalmheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,64 @@ std::string WebServer::recieveData()
 ServerSocket &WebServer::getConnection()
 {
 	return (connection);
+}
+
+Request WebServer::receiveRequest(std::string rawRequest) {
+    Request request(rawRequest);
+    if (!request.validate()) {
+        sendResponse(Response(400));
+    }
+    return (request);
+}
+
+void WebServer::sendResponse(const Response &response) { 
+    std::string rawResponse = response.getRawMessage();
+    // @todo: SEND HERE
+}
+
+Response WebServer::handleRequest(const Request &request) {
+    switch (Request::methodEnum(request.getHttpMethod())) {
+        case GET:
+            return (handleGet(request));
+        case POST:
+            return (handlePost(request));
+        case PUT:
+            return (handlePut(request));
+        case DELETE:
+            return (handleDelete(request));
+        case HEAD:
+            return (handleHead(request));
+        default:
+            return (Response(501));
+    }
+}
+
+Response WebServer::handleGet(const Request &request) {
+    Response response(200);
+    // @todo
+    return (response);
+}
+
+Response WebServer::handlePost(const Request &request) {
+    Response response(200);
+    // @todo
+    return (response);
+}
+
+Response WebServer::handlePut(const Request &request) {
+    Response response(200);
+    // @todo
+    return (response);
+}
+
+Response WebServer::handleDelete(const Request &request) {
+    Response response(200);
+    // @todo
+    return (response);
+}
+
+Response WebServer::handleHead(const Request &request) {
+    Response response(200);
+    // @todo
+    return (response);
 }
