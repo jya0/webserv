65ef48de79c93f3b1176333f223cf8743de805e6
Jinxiu Yao <jyao@student.42abudhabi.ae>
2023-09-16 15:12:08 +0400

    docs(ServerConfig): add documentation for DirectiveSimple.cpp

--- srcs/ServerConfig/DirectiveSimple.cpp
+++ srcs/ServerConfig/DirectiveSimple.cpp
@@ -6,24 +6,43 @@
 /*   By: jyao <jyao@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 14:11:12 by jyao              #+#    #+#             */
-/*   Updated: 2023/09/02 14:15:15 by jyao             ###   ########.fr       */
+/*   Updated: 2023/09/16 15:11:12 by jyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"DirectiveSimple.hpp"
 
+/**
+ * @brief Construct a new Directive Simple:: Directive Simple object
+ * 
+ */
 DirectiveSimple::DirectiveSimple(void)
 {
 }
 
+/**
+ * @brief Construct a new Directive Simple:: Directive Simple object (copy constructor)
+ * 
+ * @param simpleREF 
+ */
 DirectiveSimple::DirectiveSimple(DirectiveSimple const &simpleREF): ADirective(simpleREF)
 {
 }
 
+/**
+ * @brief Destroy the Directive Simple:: Directive Simple object
+ * 
+ */
 DirectiveSimple::~DirectiveSimple(void)
 {
 }
 
+/**
+ * @brief Assignment operator used to copy DirectiveSimple objects
+ * 
+ * @param simpleREF the reference to another DirectiveSimple object
+ * @return DirectiveSimple& 
+ */
 DirectiveSimple	&DirectiveSimple::operator=(DirectiveSimple const &simpleREF)
 {
 	_dveName = simpleREF.getName();
