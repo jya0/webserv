6217f7c3c7340a69f39582b8a03bcfaea16e1118
Jinxiu Yao <jyao@student.42abudhabi.ae>
2023-09-16 15:50:52 +0400

    docs(ServerConfig): add documentation for HTTPServerParser.hpp and .cpp

--- srcs/ServerConfig/HTTPServerParser.cpp
+++ srcs/ServerConfig/HTTPServerParser.cpp
@@ -6,13 +6,25 @@
 /*   By: jyao <jyao@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 14:44:39 by jyao              #+#    #+#             */
-/*   Updated: 2023/09/15 19:55:16 by jyao             ###   ########.fr       */
+/*   Updated: 2023/09/16 15:50:11 by jyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"HTTPServerParser.hpp"
 #include 	<utility>
 
+# define	SPACE_CHARSET	" \f\n\r\t\v"
+
+/**
+ * @brief A static function which takes the tokens and fills it
+ * into a simple directive with only ADirective::_dveName 
+ * and ADirective::_dveValues filled
+ * its ADirective::_dveType will be filled later in 
+ * ADirective::parserDirective(void)
+ * 
+ * @param tokens vector of strings gotten from tokenize(const std::string &lineREF)
+ * @return ADirective* 
+ */
 static ADirective	*tokenToDirective(std::vector<std::string> &tokens)
 {
 	DirectiveSimple	*directive;
@@ -25,6 +37,15 @@ static ADirective	*tokenToDirective(std::vector<std::string> &tokens)
 	return (directive);
 }
 
+/**
+ * @brief simply counts how many leading tab character there are 
+ * before the first non white space character, this will be used as that
+ * block directive's tab level
+ * 
+ * @param lineCnt the line read from std::getline()
+ * @return int
+ * returns 0 by default because it means there are no leading tab characters
+ */
 static int	countLeadingTabs(std::string lineCnt)
 {
 	std::size_t	tabCount;
@@ -35,8 +56,22 @@ static int	countLeadingTabs(std::string lineCnt)
 	return (0);
 }
 
-static std::streampos	getParesableLine(\
-std::ifstream &configIF, std::string &lineREF)
+/**
+ * @brief loop calls std::getline() until a line that's not empty
+ * or is filled entirely with white-spaces
+ * 
+ * @param configIF is the infile stream object used by std::getline()
+ * @param lineREF is the string reference used to store the values outside
+ * of this function's scope
+ * 
+ * @return std::streampos
+ * returns the saved stream position before 
+ * the point when it gets a parseable line.
+ * when this streampos is used it can get the same line again.
+ * basically it allows us to peek at the next line without changing
+ * the stream buffer position
+ */
+static std::streampos	getParesableLine(std::ifstream &configIF, std::string &lineREF)
 {
 	std::string			tmpLine;
 	std::streampos		befParesableLine;
@@ -48,6 +83,13 @@ std::ifstream &configIF, std::string &lineREF)
 	return (befParesableLine);
 }
 
+/**
+ * @brief uses istringstream to get separate words delimited
+ * by white-spaces in a sentence
+ * 
+ * @param lineREF the line you want to split up in to a vector of strings
+ * @return std::vector<std::string> 
+ */
 static std::vector<std::string>	tokenize(const std::string &lineREF)
 {
 	std::vector<std::string>	tmpTokens;
@@ -64,6 +106,17 @@ static std::vector<std::string>	tokenize(const std::string &lineREF)
 	return (tmpTokens);
 }
 
+/**
+ * @brief creates a new DirectiveBlock object for the return 
+ * of the first getNextDirectiveBlock(std::ifstream &configIF) call
+ * this will return a DirectiveBlock of the server directive
+ * 
+ * @param configIF is the infile stream object used by std::getline()
+ * @param tabBlockCountREF used to store block directive's tab level
+ * outside of the scope of this function.
+ * 
+ * @return DirectiveBlock* 
+ */
 static DirectiveBlock	*initDirectiveBlock(std::ifstream &configIF, int &tabBlockCountREF)
 {
 	std::string						line;
@@ -81,6 +134,18 @@ static DirectiveBlock	*initDirectiveBlock(std::ifstream &configIF, int &tabBlock
 	return (dveBlock);
 }
 
+/**
+ * @brief a recursive function which will load everything into a multimap
+ * contained by a DirectiveBlock
+ * 
+ * The meat of the recursion has 2 parts.
+ * part 1 will just retrieve single line simple directive
+ * 
+ * part 2 will call recursively to retrieve a multi-line block directive
+ * 
+ * @param configIF is the infile stream object used by std::getline()
+ * @return DirectiveBlock* 
+ */
 static DirectiveBlock	*getNextDirectiveBlock(std::ifstream &configIF)
 {
 	int								tabBlockCount;
@@ -129,8 +194,19 @@ static DirectiveBlock	*getNextDirectiveBlock(std::ifstream &configIF)
 	return (dveBlock);
 }
 
+/**
+ * @brief initialise dveNames which is used later for ADirective::parseDirective(void) to
+ * assign the correct ADirective::_dveType
+ * 
+ */
 const std::vector<std::string> HTTPServerParser::dveNames = tokenize(SIMPLE_DIRECTIVES BLOCK_DIRECTIVES);
 
+/**
+ * @brief used to return a vector of server block directives
+ * 
+ * @param filename the path to the webserver config file
+ * @return std::vector< DirectiveBlock * > 
+ */
 std::vector< DirectiveBlock * >	HTTPServerParser::parseConfigFile(std::string filename)
 {
 	DirectiveBlock					*dveBlock;
