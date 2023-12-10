a1ffb1650f8d20224dafab92544776b1d0979536
Radi <radiriyas@gmail.com>
2023-12-11 02:03:26 +0400

    feat: improve main poll control flow

--- srcs/server/ServerMonitor.cpp
+++ srcs/server/ServerMonitor.cpp
@@ -6,7 +6,7 @@
 /*   By: rriyas <rriyas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:53:34 by rriyas            #+#    #+#             */
-/*   Updated: 2023/12/09 18:13:35 by rriyas           ###   ########.fr       */
+/*   Updated: 2023/12/11 01:59:54 by rriyas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,14 +49,14 @@ void ServerMonitor::monitorCGI()
 	for (size_t i = 0; i < _cgiScripts.size(); i++)
 	{
 		curr_time = std::clock();
-		if ((size_t)(curr_time - _cgiScripts[i].getStartTime()) >= ((size_t)(CLOCKS_PER_SEC) * TIME_OUT_SEC))
+		if ((size_t)(curr_time - _cgiScripts[i].getStartTime()) >= ((size_t)(CLOCKS_PER_SEC)*TIME_OUT_SEC))
 		{
 			kill(_cgiScripts[i].getChildPid(), SIGKILL);
 			clientSock = _cgiScripts[i].getClientSocket();
 			serverSock = _cgiScripts[i].getServerSocket();
 			_cgiScripts.erase(_cgiScripts.begin() + i);
-			* _servers.at(serverSock)->responses[clientSock] = Response(408);
-			return ;
+			*_servers.at(serverSock)->responses[clientSock] = Response(408);
+			return;
 		}
 		else
 		{
@@ -68,88 +68,101 @@ void ServerMonitor::monitorCGI()
 			{
 				_servers.at((_cgiScripts[i].getServerSocket()))->closeCGI(_cgiScripts[i]);
 				_cgiScripts.erase(_cgiScripts.begin() + i);
-				return ;
+				return;
 			}
 		}
 	}
 }
 
-void ServerMonitor::startServers()
+void ServerMonitor::startListeningFromServers()
 {
-
-	int i = 0;
 	for (std::map<int, WebServer *>::iterator itr = _servers.begin(); itr != _servers.end(); itr++)
 	{
 		_sockets.addFd(itr->second->getConnection().getPassiveSocket(), POLLIN | POLLOUT);
 		itr->second->startListening();
 	}
+}
+
+bool ServerMonitor::incomingConnectiontoServer(int triggered)
+{
+	return (_servers.find(triggered) != _servers.end());
+}
+
+void ServerMonitor::acceptIncomingConnection(int triggered)
+{
+	int client;
+
+	client = _servers.find(triggered)->second->acceptConnection();
+	if (client != -1)
+		_sockets.addFd(client, POLLIN | POLLOUT | POLLHUP | POLLERR);
+}
+
+void ServerMonitor::closeClientConenction(int server, int triggered)
+{
+	_servers.at(server)->closeClientConnection(triggered);
+	_sockets.removeFd(triggered);
+}
+
+void ServerMonitor::serveClientRequest(int server, int triggered)
+{
+	_servers.at(server)->recieveData(triggered);
+	if (triggered == -1)
+		return;
+	if (_servers.at(server)->requestReady(triggered))
+	{
+		try
+		{
+			_servers.at(server)->buildResponse(triggered);
+		}
+		catch (http::CGIhandler cgi)
+		{
+			cgi.setServerSocket(server);
+			cgi.setClientSocket(triggered);
+			_cgiScripts.push_back(cgi);
+			_servers.at(server)->responses[triggered]->setResponseStatus(false);
+		}
+		std::map<int, Request *>::iterator itr = _servers.at(server)->requests.find(triggered);
+		_servers.at(server)->requests.erase(itr);
+	}
+}
+
+void ServerMonitor::serveClientResponse(int server, int client)
+{
+	if (server == -1 || _servers.at(server)->responseReady(client) == false)
+		return;
+	_servers.at(server)->sendResponse(client, *(_servers.at(server)->responses[client]));
+
+	std::map<int, Response *>::iterator response = _servers.at(server)->responses.find(client);
+	_servers.at(server)->responses.erase(response);
+}
+
+void ServerMonitor::startServers()
+{
+	startListeningFromServers();
 	int rc = 0;
 	int triggered = 0;
-	int client = 0;
 	int server = 0;
 
-	i = 0;
+	int i = 0;
 	while (2)
 	{
 		rc = _sockets.callPoll();
 		if (rc < 0)
-		{
-			perror("poll() failed");
-			break;
-		}
-
+			throw ServerSocket::SocketIOError();
 		for (i = 0; i < _sockets.getNfds(); i++)
 		{
 			triggered = _sockets[i].fd;
-			server = retrieveClientHandlerSocket(triggered);
-			if (_sockets[i].revents == 0)
+			if (_sockets[i].revents == 0 || triggered == -1)
 				continue;
-			else if (server != -1 && ((_sockets[i].revents & POLLHUP) || (_sockets[i].revents & POLLERR)))
-			{
-				_servers.at(server)->closeClientConnection(triggered);
-				_sockets.removeFd(triggered);
-			}
+			server = retrieveClientHandlerSocket(triggered);
+			if ((_sockets[i].revents & POLLHUP) || (_sockets[i].revents & POLLERR))
+				closeClientConenction(server, triggered);
+			else if (_sockets[i].revents & POLLIN && incomingConnectiontoServer(triggered))
+				acceptIncomingConnection(triggered);
 			else if (_sockets[i].revents & POLLIN)
-			{
-				if (_servers.find(triggered) != _servers.end())
-				{
-					client = _servers.find(triggered)->second->acceptConnection();
-					if (client != -1)
-						_sockets.addFd(client, POLLIN | POLLOUT | POLLHUP | POLLERR);
-				}
-				else
-				{
-					_servers.at(server)->recieveData(triggered);
-					if (triggered == -1)
-						continue;
-					else
-					{
-						if (_servers.at(server)->requestReady(triggered))
-						{
-							try
-							{
-								_servers.at(server)->buildResponse(triggered);
-							}
-							catch (http::CGIhandler cgi)
-							{
-								cgi.setServerSocket(server);
-								cgi.setClientSocket(triggered);
-								_cgiScripts.push_back(cgi);
-								_servers.at(server)->responses[triggered]->setResponseStatus(false);
-							}
-							std::map<int, Request *>::iterator itr = _servers.at(server)->requests.find(triggered);
-							_servers.at(server)->requests.erase(itr);
-						}
-					}
-				}
-			}
-			else if (server != -1 && (_sockets[i].revents & POLLOUT) && _servers.at(server)->responseReady(triggered))
-			{
-				std::cerr<<"\n\n*************************************************SENDING RESPONSE NOW!!\n\n";
-				_servers.at(server)->sendResponse(triggered, *(_servers.at(server)->responses[triggered]));
-				std::map<int, Response *>::iterator itr = _servers.at(server)->responses.find(triggered);
-				_servers.at(server)->responses.erase(itr);
-			}
+				serveClientRequest(server, triggered);
+			else if ((_sockets[i].revents & POLLOUT))
+				serveClientResponse(server, triggered);
 		}
 		monitorCGI();
 	}
